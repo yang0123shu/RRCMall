5446a30801dc4945f236a54540f8773f8954296b
阳书成 <yang0123shu@163.com>
2017-07-06 21:05:41 +0800

    Initial Commit

---
+++ RRCMall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RRCMall
+//
+//  Created by 阳书成 on 2017/7/6.
+//  Copyright © 2017年 shenzhenRRC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RRCMall/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RRCMall
+//
+//  Created by 阳书成 on 2017/7/6.
+//  Copyright © 2017年 shenzhenRRC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
